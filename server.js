3f87ad17f798cfb2d7ad9ed005a476adde139467
yonikt <yoni24ster@gmail.com>
2020-03-05 15:32:16 +0200

    df

--- server.js
+++ server.js
@@ -8,17 +8,11 @@ const mongoose = require('mongoose')
 const cheerio = require('cheerio');
 
 app.use(express.static(path.join(__dirname, 'build')));
-//app.use(bodyParser.json())
-//app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'node_modules')))
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-  
-    next()
-  })
+
 
 
 
